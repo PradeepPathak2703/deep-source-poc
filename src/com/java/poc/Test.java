b3bb14ae96c9d391294a5f8afcff400e24870800
PradeepPathak2703 <pradeep.pathak@citiustech.com>
2022-02-24 15:07:14 +0530

    Deep Source Test

---
+++ src/com/java/poc/Test.java
@@ -0,0 +1,18 @@
+package com.java.poc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		List<String> lstStr = null;
+		
+		for(int i = 1 ; i> 5 ; i++) {
+			lstStr = new ArrayList<>();
+		}
+		
+	}
+
+}
